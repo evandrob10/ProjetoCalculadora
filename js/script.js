32b3dcace2b6ca18e6b536797965ed9c05e659e2
Evandro Barros <evandrobarrosb98@gmail.com>
2023-05-29 17:12:57 -0300

    Incrementação do botão off, correção botão não numerico

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 let todosBotoes = document.querySelectorAll("tbody td");
 let monitor = document.querySelector(".monitor th p");
 let resultado = 0;
-let statusCalculado = false; // True = "Ligada" , False = "Desligada"
+let statusCalculado = false; // True = "Ligada" , False = "Desligada";
 
+let botaoOff = document.querySelector(".off");
+
+botaoOff.addEventListener("click", ()=>{
+    if(statusCalculado){
+        statusCalculado = false;
+        monitor.innerHTML = "";
+        resultado = 0;
+    }else{
+        statusCalculado = true;
+    }
+})
 
 todosBotoes.forEach( botoes=> botoes.addEventListener("click", () => botoesNumericos(botoes.innerHTML)))
 let botoesNumericos = botao => {
-    if(!isNaN(botao / 2)) { 
+    if(!isNaN(botao / 2) && statusCalculado) { 
         if(Number(monitor.innerHTML) !== resultado) { 
             monitor.innerHTML += botao 
         }else{ 
             monitor.innerHTML = ""; 
             monitor.innerHTML += botao;
         }
-    }else{
+    }else if(statusCalculado){
         botoesNaoNumericos(botao);
     }
 };
